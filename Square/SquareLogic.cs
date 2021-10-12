c6d3e7b9f900e85ed64ac28339ea5b5803586e4c
Akulin <akulinaa2002@gmail.com>
2021-10-12 16:55:43 +0800

    Добавил класс логики и тесты

---
+++ Square/SquareLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    public enum MeasureType { m2, га, а, д };
+
+    public class SquareLogic
+    {
+        private double value;
+        private MeasureType type;
+
+        public SquareLogic(double value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.m2:
+                    typeVerbose = "m2.";
+                    break;
+                case MeasureType.га:
+                    typeVerbose = "га.";
+                    break;
+                case MeasureType.а:
+                    typeVerbose = "а.";
+                    break;
+                case MeasureType.д:
+                    typeVerbose = "д.";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+    }
+}
---
+++ SquareTests/SquareLogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Square;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square.Tests
+{
+    [TestClass()]
+    public class SquareLogicTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            // тестируем все четыре типа
+            var length = new SquareLogic(38, MeasureType.m2);
+            Assert.AreEqual("38 m2.", length.Verbose());
+
+            length = new SquareLogic(38, MeasureType.га);
+            Assert.AreEqual("38 га.", length.Verbose());
+
+            length = new SquareLogic(38, MeasureType.а);
+            Assert.AreEqual("38 а.", length.Verbose());
+
+            length = new SquareLogic(38, MeasureType.д);
+            Assert.AreEqual("38 д.", length.Verbose());
+        }
+    }
+}
