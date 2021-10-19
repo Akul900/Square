33115195def8de3b882c3d7fa75da7b78b551d68
Akulin <akulinaa2002@gmail.com>
2021-10-19 16:22:32 +0800

    Тесты и вычисления

--- Square/SquareLogic.cs
+++ Square/SquareLogic.cs
@@ -38,5 +38,125 @@ namespace Square
             }
             return String.Format("{0} {1}", this.value, typeVerbose);
         }
+        public static SquareLogic operator +(SquareLogic instance, double number)
+        {
+            // расчитываем новую значение
+            var newValue = instance.value + number;
+            // создаем новый экземпляр класса, с новый значением и типом как у меры, к которой число добавляем
+            var square = new SquareLogic(newValue, instance.type);
+            // возвращаем результат
+            return square;
+        }
+
+        // чтобы можно было добавлять число также слева
+        public static SquareLogic operator +(double number, SquareLogic instance)
+        {
+            // вызываем с правильным порядком аргументов, то есть сначала длина потом число
+            // для такого порядка мы определили оператор выше
+            return instance + number;
+        }
+        public static SquareLogic operator *(SquareLogic instance, double number)
+        {
+            // мне лень по три строчки писать, поэтому я сокращаю код до одной строки
+            return new SquareLogic(instance.value * number, instance.type); ;
+        }
+
+        public static SquareLogic operator *(double number, SquareLogic instance)
+        {
+            return instance * number;
+        }
+
+        // вычитание
+        public static SquareLogic operator -(SquareLogic instance, double number)
+        {
+            return new SquareLogic(instance.value - number, instance.type); ;
+        }
+
+        public static SquareLogic operator -(double number, SquareLogic instance)
+        {
+            return instance - number;
+        }
+
+        // деление
+        public static SquareLogic operator /(SquareLogic instance, double number)
+        {
+            return new SquareLogic(instance.value / number, instance.type); ;
+        }
+
+        public static SquareLogic operator /(double number, SquareLogic instance)
+        {
+            return instance / number;
+        }
+        public SquareLogic To(MeasureType newType)
+        {
+            // по умолчанию новое значение совпадает со старым
+            var newValue = this.value;
+            // если текущий тип -- это метр
+            if (this.type == MeasureType.m2)
+            {
+                // а теперь рассматриваем все другие ситуации
+                switch (newType)
+                {
+                    // если конвертим в метр2, то значение не меняем
+                    case MeasureType.m2:
+                        newValue = this.value;
+                        break;
+                    // если в га.
+                    case MeasureType.га:
+                        newValue = this.value / 1000;
+                        break;
+                    // если в  а.
+                    case MeasureType.а:
+                        newValue = this.value / 149597870700;
+                        break;
+                    // если в десятину
+                    case MeasureType.д:
+                        newValue = this.value / (3.0856776 * Math.Pow(10, 16));
+                        break;
+                }
+            }
+            else if (newType == MeasureType.m2) // если новый тип: метр
+            {
+                switch (this.type) // а тут уже старый тип проверяем
+                {
+                    case MeasureType.m2:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.га:
+                        newValue = this.value * 1000; // кстати это то же код что и выше, только / заменили на *
+                        break;
+                    case MeasureType.а:
+                        newValue = this.value * 149597870700; // и тут / на *
+                        break;
+                    case MeasureType.д:
+                        newValue = this.value * (3.0856776 * Math.Pow(10, 16)); // и даже тут, просто / на *
+                        break;
+                }
+            }
+            else // то есть не в метр и не из метра
+            {
+                newValue = this.To(MeasureType.m2).To(newType).value;
+                // в принципе можно сразу написать 
+                // return this.To(MeasureType.m).To(newType);
+                // но хорошем тоном считается наличие всего одного return в функции
+            }
+            return new SquareLogic(newValue, newType);
+        }
+        public static SquareLogic operator +(SquareLogic instance1, SquareLogic instance2)
+        {
+            // то есть у текущей длине добавляем число 
+            // полученное преобразованием значения второй длины в тип первой длины
+            // так как у нас определен operator+(Length instance, double number)
+            // то это сработает как ожидается
+            return instance1 + instance2.To(instance1.type).value;
+        }
+
+        // вычитание двух длин
+        public static SquareLogic operator -(SquareLogic instance1, SquareLogic instance2)
+        {
+            // тут все тоже, только с минусом
+            return instance1 - instance2.To(instance1.type).value;
+        }
     }
+
 }
--- SquareTests/SquareLogicTests.cs
+++ SquareTests/SquareLogicTests.cs
@@ -27,5 +27,76 @@ namespace Square.Tests
             length = new SquareLogic(38, MeasureType.д);
             Assert.AreEqual("38 д.", length.Verbose());
         }
+
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var square = new SquareLogic(1, MeasureType.m2);
+            square = square + 4.25;
+            Assert.AreEqual("5,25 m2.", square.Verbose());
+        }
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var length = new SquareLogic(3, MeasureType.m2);
+            length = length - 1.75;
+            Assert.AreEqual("1,25 m2.", length.Verbose());
+        }
+
+        [TestMethod()]
+        public void MulByNumberTest()
+        {
+            var length = new SquareLogic(3, MeasureType.m2);
+            length = length * 3;
+            Assert.AreEqual("9 m2.", length.Verbose());
+        }
+
+        [TestMethod()]
+        public void DivByNumberTest()
+        {
+            var length = new SquareLogic(3, MeasureType.m2);
+            length = length / 3;
+            Assert.AreEqual("1 m2.", length.Verbose());
+        }
+        [TestMethod()]
+        public void MeterToAnyTest()
+        {
+            SquareLogic square;
+
+            square = new SquareLogic(1000, MeasureType.m2);
+            Assert.AreEqual("1 га.", square.To(MeasureType.га).Verbose());
+
+            square = new SquareLogic(149597870700 * 2, MeasureType.m2);
+            Assert.AreEqual("2 а.", square.To(MeasureType.а).Verbose());
+
+            square = new SquareLogic(3 * 3.0856776 * Math.Pow(10, 16), MeasureType.m2);
+            Assert.AreEqual("3 д.", square.To(MeasureType.д).Verbose());
+        }
+        [TestMethod()]
+        public void AnyToMeterTest()
+        {
+            SquareLogic square;
+
+            square = new SquareLogic(1, MeasureType.га);
+            Assert.AreEqual("1000 m2.", square.To(MeasureType.m2).Verbose());
+
+            square = new SquareLogic(1, MeasureType.а);
+            Assert.AreEqual("149597870700 m2.", square.To(MeasureType.m2).Verbose());
+
+            square = new SquareLogic(1, MeasureType.д);
+            Assert.AreEqual("3,0856776E+16 m2.", square.To(MeasureType.m2).Verbose());
+        }
+        [TestMethod()]
+        public void AddSubKmMetersTest()
+        {
+            var m2 = new SquareLogic(100, MeasureType.m2);
+            var ga = new SquareLogic(1, MeasureType.га);
+
+            Assert.AreEqual("1100 m2.", (m2 + ga).Verbose());
+            Assert.AreEqual("1,1 га.", (ga + m2).Verbose());
+
+            Assert.AreEqual("0,9 га.", (ga - m2).Verbose());
+            Assert.AreEqual("-900 m2.", (m2 - ga).Verbose());
+        }
     }
 }
